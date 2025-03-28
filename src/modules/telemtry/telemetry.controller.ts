68959ae738c1cdff19cb0cd148b394980d367a94
Arati Tekdi <arati.kulkarni@tekditechnologies.com>
2025-03-28 16:24:10 +0530

    Added API to add telemetry event from learner app.

---
+++ src/modules/telemtry/telemetry.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body, Req, Res } from '@nestjs/common';
+import { TelemetryService } from './telemetry.service';
+import {
+  ApiOkResponse,
+  ApiInternalServerErrorResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
+import { Telemetry } from './entities/telemetry';
+
+@Controller('telemetry')
+export class TelemetryController {
+  constructor(private readonly telemetryService: TelemetryService) {}
+
+  @ApiOkResponse({ description: 'Telemetry log created successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error.' })
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
+  @Post()
+  async addEventLog(
+    @Body() createTelemetryDto: Partial<Telemetry>,
+    @Res() response: Response,
+    @Req() request: Request,
+  ) {
+    return this.telemetryService.addTelemetryLog(
+      createTelemetryDto,
+      response,
+      request,
+    );
+  }
+}
